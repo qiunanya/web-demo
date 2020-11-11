1cb4e59328e873cef07a663df3953da55611855f
qiunanya <1766226354@qq.com>
2020-11-11 17:29:39 +0800

    添加联系地址

--- web-vue-list/src/router/index.js
+++ web-vue-list/src/router/index.js
@@ -1,45 +1,44 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/home/Home.vue'
-import w3cStandard from '@/views/templates/w3c-standard'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-    children:[
-        {
-            path: '/w3c',
-            name: 'w3c',
-            component: () => import('@/views/templates/w3c-standard'),
-            meta:{
-                title:'w3c html5 标准'
-            }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+        meta:{
+            title:'首页',
+            icon: 'ios-people'
         },
-        {
-            path: '/seletMenu',
-            name: 'seletMenu',
-            component: () => import('@/views/templates/select-menu'),
-            meta:{
-                title: '浮动下拉菜单'
+        children:[
+            {
+                path: '/w3c',
+                name: 'w3c',
+                component: () => import('@/views/templates/w3c-standard'),
+                meta:{
+                    title:'w3c html5 标准'
+                }
+            },
+            {
+                path: '/seletMenu',
+                name: 'seletMenu',
+                component: () => import('@/views/templates/select-menu'),
+                meta:{
+                    title: '浮动下拉菜单'
+                }
+            },
+            {
+                path: '/test',
+                name: 'test',
+                component: () => import('@/views/templates/test'),
+                meta:{
+                    title: '测试页面'
+                }
             }
-        },
-        {
-            path: '/test',
-            name: 'test',
-            component: () => import('@/views/templates/test'),
-            meta:{
-                title: '测试页面'
-            }
-        }
-    ],
-    meta:{
-        title:'首页',
-        icon: 'ios-people'
-    }
+        ],
     },
     {
         path: '/about',
@@ -47,11 +46,21 @@ const routes = [
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+        component: Home,
         meta:{
             title:'关于项目',
             icon: 'ios-stats'
-        }
+        },
+        children:[
+            {
+                path: '/readme',
+                name: 'readme',
+                component: () => import('@/views/templates/readme'),
+                meta:{
+                    title: '项目地址'
+                }
+            }
+        ]
     }
 ]
 
---
+++ web-vue-list/src/views/templates/readme.vue
@@ -0,0 +1,26 @@
+<template >
+    <div style="color:#2d8cf0;">
+        <h3>项目地址：https://github.com/qiunanya/web-demo/tree/dev</h3>
+        <h3>CSDN博客地址：https://blog.csdn.net/qq_41444226</h3>
+    </div>
+</template>
+<script>
+export default {
+    name:'readme',
+    components:{},
+    data() {
+        return {
+            
+        }
+    },
+    created() {
+        
+    },
+    methods: {
+        
+    },
+    mounted() {
+        
+    },
+}
+</script>
