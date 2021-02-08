6fbb8315d9558c3868448c1638bd173c2a367eef
邱南亚 <1766226354@qq.com>
2021-02-08 10:52:56 +0800

    更换为element-ui框架

--- web-element-ui-demo/src/main.js
+++ web-element-ui-demo/src/main.js
@@ -2,11 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ViewUI from 'view-design';
-import 'view-design/dist/styles/iview.css';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.use(ViewUI)
-
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
