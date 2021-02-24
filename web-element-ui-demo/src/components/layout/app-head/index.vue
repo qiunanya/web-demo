38faf8647496b1418cb619d0ebd1bd81bb3ffbc6
邱南亚 <1766226354@qq.com>
2021-02-24 10:24:56 +0800

    更改菜单模式左侧显示

--- web-element-ui-demo/src/components/layout/app-head/index.vue
+++ web-element-ui-demo/src/components/layout/app-head/index.vue
@@ -1,29 +1,7 @@
 <template>
-	<div class="wrapper">
-		<el-menu
-			:default-active="activeIndex2"
-			class="el-menu-demo"
-			mode="horizontal"
-			background-color="#545c64"
-			text-color="#fff"
-			active-text-color="#ffd04b"
-		>
-            <template v-for="(item, index) in menuList">
-			    <el-menu-item :index="String(index+1)" :key="index" v-if="!item.children" @click="handleSelect(item, index)">{{ item.meta.title }}</el-menu-item>
-                <el-submenu v-else :key="index" :index="String(index+1)">
-                    <template slot="title">{{ item.meta.title }}</template>
-                    <el-menu-item :index="`${index+1}-${subIndex+1}`" v-for="(subItem, subIndex) in item.children" :key="subIndex" @click="handleSelect(subItem, subIndex)">{{ subItem.meta.title }}</el-menu-item>
-                    <!-- <el-menu-item index="2-2">选项2</el-menu-item>
-                    <el-menu-item index="2-3">选项3</el-menu-item> -->
-                    <!-- <el-submenu index="2-4">
-                        <template slot="title">选项4</template>
-                        <el-menu-item index="2-4-1">选项1</el-menu-item>
-                        <el-menu-item index="2-4-2">选项2</el-menu-item>
-                        <el-menu-item index="2-4-3">选项3</el-menu-item>
-                    </el-submenu> -->
-                </el-submenu>
-            </template>
-		</el-menu>
+	<div class="head_wrapper">
+        <img src="@/assets/logo.png">
+		<h2>JavaScript For Function</h2>
 	</div>
 </template>
 
@@ -46,7 +24,6 @@ export default {
 	},
     mounted() {
         this.menuList = this.$router.options.routes[0].children
-        console.log(this.$router.options.routes[0].children,5555)
     },
 };
 </script>
--- web-element-ui-demo/src/components/layout/index.vue
+++ web-element-ui-demo/src/components/layout/index.vue
@@ -1,7 +1,16 @@
 <template>
-    <div class="wrapper">
-        <app-head></app-head>
-        <router-view></router-view>
+    <div class="app_wrapper">
+        <div class="header">
+            <app-head></app-head>
+        </div>
+        <div class="app_content">
+            <div class="menu">
+                <app-menu></app-menu>
+            </div>
+            <div class="content">
+                <router-view></router-view>
+            </div>
+        </div>
         <app-foot></app-foot>
     </div>
 </template>
@@ -10,12 +19,14 @@
 // @ is an alias to /src
 import AppFoot from '@/components/layout/app-foot'
 import AppHead from '@/components/layout/app-head'
+import AppMenu from '@/components/layout/app-menu'
 
 export default {
   name: "Home",
   components: {
       AppHead,
-      AppFoot
+      AppFoot,
+      AppMenu
   },
   data() {
     return {};
@@ -23,3 +34,6 @@ export default {
   methods: {}
 };
 </script>
+<style lang="scss">
+@import url('./style.scss');
+</style>
